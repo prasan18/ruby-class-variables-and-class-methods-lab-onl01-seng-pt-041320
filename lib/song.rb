bffb80c74851d49fb215863630bb62a8bc78db59
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-06 00:49:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,24 @@
-
+class Song 
+  @@count = 0
+  
+  def self.count 
+    @@count
+  end 
+  
+  # Control for duplicates when you code your 
+  # .genres class method, not when you add 
+  # genres to the original @@genres array.
+  @@genres = []
+  
+# this should be deleted at some point
+# def self.total_genre_entries
+#   @@genres
+# end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
 def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
