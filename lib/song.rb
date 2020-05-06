e30653fdf63833a94b8b236a557a8989b112b83f
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-06 00:46:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,2 @@
-class Song 
-  attr_accessor :name, :artist, :genre
-  def initiliaze(name,artist,genre)
-    @name = name 
-    @artist = artist
-    @genre = genre
-  end
-end
 
-   class Song
-     def song.count
-  @@count = 0 
- 
-  def initialize
-    @@count += 1
-  end
- 
-  def song.count
-    @@count
-  end
-end
     
